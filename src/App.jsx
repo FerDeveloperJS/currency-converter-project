0681fd5aa170947bb8061b18792a6ec4933507dc
FerDeveloperJS <ferchosoftwaredev@gmail.com>
2025-09-26 22:39:17 -0500

    Added some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ArrowLeftRight from "./assets/svg/ArrowLeftRightSvg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="min-h-screen p-4 flex justify-center items-center bg-[#4e148c]">
+      <div className="bg-white p-10 rounded-xl">
+        <h1 className="text-3xl font-semibold mb-10">
+          Currency Converter By Fer
+        </h1>
+        <div className="flex flex-col gap-2.5 mb-10">
+          <label htmlFor="enter-amount" className="text-xl">
+            Ingresar cantidad
+          </label>
+          <input
+            id="enter-amount"
+            type="text"
+            className="border border-gray-400 p-2 rounded-xl"
+          />
+        </div>
+
+        <div className="flex justify-around items-center mb-14">
+          <div className="flex flex-col gap-1.5 items-center">
+            <label className="text-xl" htmlFor="from">
+              De
+            </label>
+            <select
+              id="from"
+              className="p-1.5 rounded-xl border border-gray-300"
+            ></select>
+          </div>
+
+          <ArrowLeftRight />
+
+          <div className="flex flex-col gap-1.5 items-center">
+            <label className="text-xl" htmlFor="to">
+              A
+            </label>
+            <select
+              id="to"
+              className="p-1.5 rounded-xl border border-gray-300"
+            ></select>
+          </div>
+        </div>
+
+        <button className="cursor-pointer p-2.5 flex items-center justify-center text-white text-2xl rounded-xl w-full bg-[#4e148c]">
+          Convertir divisa
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
